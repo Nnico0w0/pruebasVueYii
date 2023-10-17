016aceebafde54b244dc06ec0ea628042d8f815a
Nnico0w0 <n.nico0w0@gmail.com>
2023-10-17 07:58:26 -0300

    normalizacion de URLs

--- backend/basic/config/web.php
+++ backend/basic/config/web.php
@@ -45,14 +45,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'class' => 'yii\rest\UrlRule',
+                'controller' => 'apiv1',
             ],
         ],
-        */
     ],
     'modules' => [
         'apiv1' => [
